a5b79534d3ac6914e8b5dbf47e92e2667c9758a7
marsh1100 <margie.b.agu17@gmail.com>
2024-01-19 13:56:53 -0500

    feat: :sparkles: Property Binding - Event Binding

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  @Input({required : true}) img : string = '';
+  @Input({required : true}) price : number = 0;
+  @Input({required : true}) title : string = '';
+
+  @Output() addTocart = new EventEmitter();
+
+  addToCartHandler()
+  {
+    console.log('click from child');
+    this.addTocart.emit('Hola este es un mensajito desde el hijo '+ this.title)
+  }
+}
---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ProductComponent } from '../../components/product/product.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  fromChild(event: string)
+  {
+    console.log(event)
+  }
+}
