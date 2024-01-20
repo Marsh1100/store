5e711a5fa05414826b5bd5b3718a1b609afe85b0
marsh1100 <margie.b.agu17@gmail.com>
2024-01-19 20:57:53 -0500

    feat: :sparkles: Mostrar carrito de compras y el total

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { IProduct } from '../../models/product.model';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [CurrencyPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -13,9 +15,22 @@ export class HeaderComponent {
   @Input({required: true}) cart : IProduct[] = [];
 
   hideSideMenu = signal(true);
+  total = signal(0);
 
   toogleSideMenu()
   {
     this.hideSideMenu.update(prevState => !prevState);
+  }
+  ngOnChanges(change : SimpleChanges){
+    const cart = change['cart'];
+    if(cart)
+    {
+      this.total.set(this.calcTotal());
+    }
+  }
+
+  calcTotal()
+  {
+    return this.cart.reduce((total,product)=> total + product.price,0);
   }
 }
