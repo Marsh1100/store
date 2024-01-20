3de7962840631525f70b4a93e7ca08dbfc3b8401
marsh1100 <margie.b.agu17@gmail.com>
2024-01-20 17:49:09 -0500

    feat: :sparkles: Pipes personalizada

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,14 +1,15 @@
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '@shared/models/product.model';
+import { ReversePipe } from "@shared/pipes/reverse.pipe";
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [CommonModule],
-  providers: [CurrencyPipe],
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+    selector: 'app-product',
+    standalone: true,
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.css',
+    imports: [CommonModule, ReversePipe, TimeAgoPipe]
 })
 export class ProductComponent {
   @Input({required : true}) product !: IProduct;
